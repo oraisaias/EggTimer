a9b2f7c38b5ca952d3b445b43d6daf4c2bb4242d
Isaías Chávez Martínez <isaiaschavez.co@outlook.com>
2025-04-18 21:29:36 -0600

    Dictionaries

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -1,15 +1,29 @@
-//
-//  ViewController.swift
-//  EggTimer
-//
-//  Created by Angela Yu on 08/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    let add = Int.random(in: 0...100)
+    let eggTimes = ["Soft":5, "Medium":12, "Hard":7]
+    let softTime = 5
+    let mediumTime = 12
+    let hardTime = 7
+    override func viewDidLoad() {
+         super.viewDidLoad()
+         
+         print(add) // ✅ Ahora sí se puede ejecutar
+     }
     
-
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        let other = eggTimes[hardness];
+        
+        if hardness == "Soft" {
+            print("Tiempo: \(softTime) segundos")
+        } else if hardness == "Medium" {
+            print("Tiempo: \(mediumTime) segundos")
+        } else if hardness == "Hard" {
+            print("Tiempo: \(hardTime) segundos")
+        } else {
+            print("Valor no reconocido")
+        }
+    }
 }
